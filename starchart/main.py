415f5cd0749be713930b2cf9c3528a26245f6eeb
monochromegane <dev.kuro.obi@gmail.com>
2017-01-11 16:28:10 +0900

    Implemented training program packaging in train option.

--- starchart/main.py
+++ starchart/main.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from ml import trainer, exposer, applyer
+from starchart.ml import trainer, exposer, applyer
 
 def main():
     parser = argparse.ArgumentParser(description='StarChart')
     subparsers = parser.add_subparsers()
 
     train_parser = subparsers.add_parser('train', help='Train model')
+    train_parser.add_argument('--project-id',     help="Project ID.")
+    train_parser.add_argument('--package-path',   help="Train program package path.")
+    train_parser.add_argument('--module-name',    help="Train program module name.")
+    train_parser.add_argument('--region',         help="Region.")
+    train_parser.add_argument('args', nargs='*',  help="Train program parameters.")
     train_parser.set_defaults(func=trainer.train)
 
     expose_parser = subparsers.add_parser('expose', help='Expose model')
--- starchart/ml/trainer.py
+++ starchart/ml/trainer.py
@@ -1,5 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
+import subprocess
+import tempfile
+import sys
+from datetime import datetime
+from contextlib import contextmanager
+
+_DEFAULT_SETUP_FILE = """\
+from setuptools import setup
+if __name__ == '__main__':
+    setup(name='{package_name}', packages=['{package_name}'])
+"""
+
 def train(args):
-    pass
+    context = Context(args)
+
+    # setup train program.
+    with _temporaryDirectory() as temp_dir:
+        dest_dir = os.path.join(temp_dir, 'dest')
+        shutil.copytree(context.setup_dir, dest_dir)
+        package_paths = _run_setup(dest_dir, context.package_name)
+
+class Context(object):
+    def __init__(self, args):
+        self.args = args
+        self.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+
+        self.bucket_name = '{}-ml'.format(args.project_id)
+        self.setup_dir, self.package_name = os.path.split(os.path.abspath(args.package_path))
+        _, self.model_name = os.path.split(self.setup_dir)
+        self.job_id     = '_'.join([self.model_name, self.timestamp])
+        self.train_dir  = '/'.join([self.model_name, self.timestamp])
+        self.train_path = '/'.join(['gs:/', self.bucket_name, self.train_dir])
+
+    def train_args(self):
+        return [arg.replace('TRAIN_PATH', self.train_path)for arg in self.args.args]
+
+def _run_setup(setup_dir, package_name):
+    setup_path = os.path.join(setup_dir, 'setup.py')
+    if not os.path.isfile(setup_path):
+        with open(setup_path, 'w') as setup_file:
+            setup_contents = _DEFAULT_SETUP_FILE.format(package_name=package_name)
+            setup_file.write(setup_contents)
+
+    args = [sys.executable, 'setup.py', 'sdist', '--dist-dir=dist']
+    subprocess.call(args, cwd=setup_dir)
+
+    dist_dir = os.path.join(setup_dir, 'dist')
+    local_paths = [os.path.join(dist_dir, rel_file)
+                   for rel_file in os.listdir(dist_dir)]
+    return local_paths
+
+@contextmanager
+def _temporaryDirectory():
+    try:
+        temp_dir = tempfile.mkdtemp()
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
