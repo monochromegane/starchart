4bef1e51710eba6c5ee6270590f9d3037b4812e9
monochromegane <dev.kuro.obi@gmail.com>
2017-01-13 16:28:54 +0900

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='starchart',
+    version='0.0.1',
+    description='StarChart is a tool to manage Google Cloud Machine Learning training programs and model versions.',
     author='monochromegane',
+    url='https://github.com/monochromegane/starchart',
     packages=find_packages(),
     install_requires=['google-cloud-storage', 'google-api-python-client'],
     entry_points={
